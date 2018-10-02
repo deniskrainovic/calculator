5e769107d0f53dd24059a5d7c8deda11ddea8f80
Denis Krainovic <denis.krainovic@outlook.at>
2018-10-02 23:23:33 +0200

    script.js add eventlistener's, calculator output

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+const panel = document.querySelector(".panel");
+const equal = document.querySelector(".equal");
+const numbers = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+
+let displayValue = "";
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = "";
+
+numbers.forEach(button => button.addEventListener("click", function(e) {
+    displayValue += e.target.innerHTML;
+    panel.innerHTML = displayValue;
+    if(firstNumber === "") {
+        firstNumber = e.target.innerHTML;
+    } else {
+        secondNumber = e.target.innerHTML;
+    }
+}))
+
+operator.forEach(button => button.addEventListener("click", function(e) {
+    displayValue += e.target.innerHTML;
+    currentOperator = e.target.innerHTML;
+}))
+
+equal.addEventListener("click", function(e) {
+    panel.innerHTML = operate(firstNumber, currentOperator, secondNumber);
+})
+
 
 let operate = (firstNum, operator, secondNum) => {
     switch (operator) {
